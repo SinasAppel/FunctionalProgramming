325d3803a534711d067e62ae1a1c49f5069e98a8
A. Tilstra <s3163962@fwn-bborg-edu-74-91.fmnsedu.rug.nl>
2017-09-14 11:24:56 +0200

    Completed happywrapper.hs

--- happywrapper.hs
+++ happywrapper.hs
@@ -1,25 +1,22 @@
+{- Insert here your own code. The type of countHappyNumbers must be:
+
 countHappyNumbers :: Integer -> Integer -> Int
-countHappyNumbers a b
- | a > b = 0
- | True  = if isHappyNumber a then 1 + countHappyNumbers (a+1) b else countHappyNumbers (a+1) b
 
-{- 
-Returns true when a is a happy number
 -}
-isHappyNumber :: Integer -> Bool
-isHappyNumber 1 = True
-isHappyNumber 4 = False
-isHappyNumber a = isHappyNumber (sumOfSquaredDigits a 0)
 
-{- 
-Returns the sum of the squared digits of an int
-Default value of b is 0, it is used as a store for the sum
--}
-sumOfSquaredDigits :: Integer -> Integer -> Integer
-sumOfSquaredDigits a b 
- | (a `div` 10 > 0) = sumOfSquaredDigits (a `div` 10) (b + (a `mod` 10)^2)
- | True             = (a*a + b)
+sumSquare :: Integer -> Integer
+sumSquare x
+  | x<10        = (x^2)
+  | otherwise   = (sumSquare (x `quot` 10) + (x `mod` 10)^2)
 
+isHappyNumber :: Integer -> [Integer] -> Bool
+isHappyNumber x y
+  | x==1          = True
+  | elem x y      = False
+  | otherwise     = isHappyNumber (sumSquare x) (y ++ [x])
+
+countHappyNumbers :: Integer -> Integer -> Int
+countHappyNumbers a b = length ([ x | x <- [a .. b], (isHappyNumber x [])])
 
 wrapper :: [String] -> Int
 wrapper (a:b:_) = countHappyNumbers (read a::Integer) (read b::Integer)
