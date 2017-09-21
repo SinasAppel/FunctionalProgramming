5660112f2d76ef2ee586c14ab6a3a543d13213fb
Jur Kroeze <jurkrz@gmail.com>
2017-09-21 12:05:15 +0200

    temporary commit

--- takuzustringswrapper.hs
+++ takuzustringswrapper.hs
@@ -1,8 +1,16 @@
-{- Insert here your own code. The type of takuzuStrings must be:
-
 takuzuStrings :: Integer-> [String]
+takuzuStrings a = getStrings a []
 
--}
+getOne :: Integer -> [String] -> [String]
+getOne 0 xs = xs
+getOne a [] = getOne (a-1) ["1"] ++ getZero (a-1) ["0"]
+getOne a (x:xs) = getOne (a-1) ([['1'] ++ x] ++ xs) ++ getZero (a-1) ([['0'] ++ x] ++ xs)
+
+getStrings :: Integer -> [String] -> [String]
+getStrings 0 xs = xs
+getStrings a [] = getStrings (a-1) ["0"] ++ getStrings (a-1) ["1"]
+getStrings a (x:xs) = getStrings (a-1) ([['0'] ++ x] ++ xs) ++ getStrings (a-1) ([['1'] ++ x] ++ xs)
+
 
 wrapper :: [String] -> [String]
 wrapper (a:_) = takuzuStrings (read a::Integer)
