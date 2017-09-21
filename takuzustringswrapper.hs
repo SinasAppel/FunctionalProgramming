1011f1617e08c7c4216ac0f9ed78ea02349a31ce
Jur Kroeze <jurkrz@gmail.com>
2017-09-21 22:48:30 +0200

    almost done with takuzustirngs

--- takuzustringswrapper.hs
+++ takuzustringswrapper.hs
@@ -1,15 +1,15 @@
 takuzuStrings :: Integer-> [String]
 takuzuStrings a = getStrings a []
 
-getOne :: Integer -> [String] -> [String]
-getOne 0 xs = xs
-getOne a [] = getOne (a-1) ["1"] ++ getZero (a-1) ["0"]
-getOne a (x:xs) = getOne (a-1) ([['1'] ++ x] ++ xs) ++ getZero (a-1) ([['0'] ++ x] ++ xs)
-
 getStrings :: Integer -> [String] -> [String]
 getStrings 0 xs = xs
 getStrings a [] = getStrings (a-1) ["0"] ++ getStrings (a-1) ["1"]
+getStrings a (x@(b:c:ys):xs)
+ | b == '1' && c == '1' = getStrings (a-1) ([['0'] ++ x] ++ xs)
+ | b == '0' && c == '0' = getStrings (a-1) ([['1'] ++ x] ++ xs)
+ | otherwise = getStrings (a-1) ([['0'] ++ x] ++ xs) ++ getStrings (a-1) ([['1'] ++ x] ++ xs)
 getStrings a (x:xs) = getStrings (a-1) ([['0'] ++ x] ++ xs) ++ getStrings (a-1) ([['1'] ++ x] ++ xs)
+
 
 
 wrapper :: [String] -> [String]
