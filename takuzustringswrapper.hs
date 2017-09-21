c67e2b3149e1a2afe588c49727a2b374081645f6
Jur Kroeze <SinasAppel@users.noreply.github.com>
2017-09-21 23:14:07 +0200

    Excercise 2 finished

--- takuzustringswrapper.hs
+++ takuzustringswrapper.hs
@@ -1,5 +1,7 @@
+import Data.List
+
 takuzuStrings :: Integer-> [String]
-takuzuStrings a = getStrings a []
+takuzuStrings a = sort (filterStrings (getStrings a []))
 
 getStrings :: Integer -> [String] -> [String]
 getStrings 0 xs = xs
@@ -10,6 +12,18 @@ getStrings a (x@(b:c:ys):xs)
  | otherwise = getStrings (a-1) ([['0'] ++ x] ++ xs) ++ getStrings (a-1) ([['1'] ++ x] ++ xs)
 getStrings a (x:xs) = getStrings (a-1) ([['0'] ++ x] ++ xs) ++ getStrings (a-1) ([['1'] ++ x] ++ xs)
 
+filterStrings :: [String] -> [String]
+filterStrings [] = []
+filterStrings (x:xs) = if isAllowedTakuzuString x then filterStrings xs ++ [x] else filterStrings xs
+
+isAllowedTakuzuString :: String -> Bool
+isAllowedTakuzuString xs = if x == y then True else False where
+ x = elemNum '0' xs
+ y = elemNum '1' xs
+
+elemNum :: Char -> String -> Integer
+elemNum y [] = 0
+elemNum y (x:xs) = if x == y then 1 + elemNum y xs else elemNum y xs
 
 
 wrapper :: [String] -> [String]
